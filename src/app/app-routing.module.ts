2deb57a1e14bce407abd2f3d7f24d3771a7d9e46
acs.gunnam <acs.gunnam@gmail.com>
2022-09-04 10:36:55 +0530

    moved files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatNativeDateModule } from '@angular/material/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MaterialExampleModule } from '../material.module';
-import { HomeComponent } from '../landing-page/home/home.component';
+import { MaterialExampleModule } from './material.module';
+import { HomeComponent } from './home/home.component';
 import { SidenavResponsiveExample } from './sidenav-responsive-example';
 import { CommonModule } from '@angular/common';
 
